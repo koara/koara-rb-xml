17ec2c01219a582e60108d4e98c5ab8de7575106
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-06-29 08:07:01 +0200

    part of the renderer

--- lib/koara/xml/xmlrenderer.rb
+++ lib/koara/xml/xmlrenderer.rb
@@ -22,7 +22,7 @@ module Koara
       def visit_heading(node)
         @level++
         indent
-        out << "<heading level=\"" + node.value + "\""
+        @out << "<heading level=\"" + node.value + "\""
         if node.children && node.children.length > 0
           @out << ">\n"
           @level += 1
@@ -36,74 +36,69 @@ module Koara
         @level -= 1
       end
 
-      #       @Override
-      #       public void visit(BlockQuote node) {
-      #         level++;
-      #         indent();
-      #         out.append("<blockquote");
-      #         if(node.getChildren() != null && node.getChildren().length > 0) {
-      #             out.append(">\n");
-      #         level++;
-      #         node.childrenAccept(this);
-      #         level--;
-      #         indent();
-      #         out.append("</blockquote>\n");
-      #         level--;
-      #         } else {
-      #             out.append(" />\n");
-      #         }
-      #         }
-      #
-      #         @Override
-      #         public void visit(ListBlock node) {
-      #           level++;
-      #           indent();
-      #           out.append("<list ordered=\"" + node.isOrdered() + "\">\n");
-      #           node.childrenAccept(this);
-      #           indent();
-      #           out.append("</list>\n");
-      #           level--;
-      #         }
-      #
-      #         @Override
-      #         public void visit(ListItem node) {
-      #           level++;
-      #           indent();
-      #           out.append("<listitem");
-      #           if(node.getNumber() != null) {
-      #               out.append(" number=\"" + node.getNumber() + "\"");
-      #           }
-      #           if(node.getChildren() != null && node.getChildren().length > 0) {
-      #               out.append(">\n");
-      #           node.childrenAccept(this);
-      #           indent();
-      #           out.append("</listitem>\n");
-      #           } else {
-      #               out.append(" />\n");
-      #           }
-      #           level--;
-      #           }
-      #
-      #           @Override
-      #           public void visit(CodeBlock node) {
-      #             level++;
-      #             indent();
-      #             out.append("<codeblock");
-      #             if(node.getLanguage() != null) {
-      #                 out.append(" language=\"" + escape(node.getLanguage()) + "\"");
-      #             }
-      #             if(node.getValue() != null && node.getValue().toString().length() > 0) {
-      #                 out.append(">");
-      #             level++;
-      #             out.append(escape(node.getValue().toString()));
-      #             level--;
-      #             out.append("</codeblock>\n");
-      #             } else {
-      #                 out.append(" />\n");
-      #             }
-      #             level--;
-      #             }
-      #
+      def visit_blockquote(node)
+        @level+=1
+        indent
+        @out << '<blockquote'
+        if node.children && node.children.length > 0
+          out << ">\n"
+          @level+=1
+          node.children_accept(self)
+          @level-=1
+          indent
+          @out << "</blockquote>\n"
+          @level-=1
+        else
+          @out << " />\n"
+        end
+      end
+
+      def visit_listblock(node)
+        @level += 1
+        indent
+        @out << "<list ordered=\"#{node.ordered}\">\n"
+        node.childre_accept(self)
+        indent
+        @out << "</list>\n"
+        @level -= 1
+      end
+
+      def visit_listitem(node)
+        @level += 1
+        indent
+        out << "<listitem"
+        if node.number
+          out << " number=\"#{node.number}\"";
+        end
+        if node.children && node.children.length > 0
+          @out << ">\n"
+          node.children_accept(self)
+          indent
+          @out << "</listitem>\n"
+        else
+          @out << " />\n"
+        end
+        @level -= 1
+      end
+
+      def visit_codeblock(node)
+        @level += 1
+        indent
+        @out << "<codeblock"
+        if node.language
+          @out << " language=\"#{escape(node.language)}\""
+        end
+        if node.value && node.value.length > 0
+          @out << ">"
+          @level += 1
+          @out << escape(node.value)
+          @level -= 1
+          @out << "</codeblock>\n"
+        else
+          @out << " />\n"
+        end
+        @level -= 1
+      end
 
       def visit_paragraph(node)
         @level += 1
@@ -121,26 +116,25 @@ module Koara
       end
 
       def visit_image(node)
-      indent
-      @out << "<image url=\"" + escape_url(node.value) + "\">\n"
-      @level += 1
-      node.children_accept(self)
-      @level -= 1
-      indent()
-      @out << "</image>\n"
+        indent
+        @out << "<image url=\"" + escape_url(node.value) + "\">\n"
+        @level += 1
+        node.children_accept(self)
+        @level -= 1
+        indent()
+        @out << "</image>\n"
       end
 
-      #             @Override
-      #             public void visit(Link node) {
-      #               indent();
-      #               out.append("<link url=\"" + escapeUrl(node.getValue().toString()) + "\">\n");
+     def visit_link(node)
+      indent
+      @out << "<link url=\"#{escape_url(node.value)}\">\n"
       #               level++;
       #               node.childrenAccept(this);
       #               level--;
       #               indent();
       #               out.append("</link>\n");
-      #             }
-      #
+     end
+
       def visit_text(node)
         indent()
         @out << '<text>'
